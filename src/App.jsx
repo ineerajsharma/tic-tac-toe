4f357022d071e711318436662176cf562f2304d7
ineerajsharma <neeraj999456@gmail.com>
2021-11-29 18:47:26 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>TIC TAC TOE</h1>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+// "@babel/eslint-parser": "^7.16.3",
+
+// "parser": "@babel/eslint-parser",
+// "parserOptions": {
+//   "requireConfigFile": false
+// }
