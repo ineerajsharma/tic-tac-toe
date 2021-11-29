8aab10347906a0715334c8c7564013ac26bfb804
ineerajsharma <neeraj999456@gmail.com>
2021-11-29 20:43:13 +0530

    Created Board and Square Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import Board from './components/Board';
 
 const App = () => {
   return (
     <div>
       <h1>TIC TAC TOE</h1>
+      <Board/>
     </div>
   );
 };
 
 export default App;
-
-
 
 // "@babel/eslint-parser": "^7.16.3",
 
