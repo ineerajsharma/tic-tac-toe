7c6be7b16a40721a8f6b907b2385a1edfdb07ac7
ineerajsharma <neeraj999456@gmail.com>
2021-12-01 22:50:21 +0530

    Styled the entire app

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 
-const statusMessage = ({ winner, current }) => {
-  //   const message = winner
-  //   ? `winner is ${winner}`
-  //   : `Next player is ${current.isXNext ? 'X' : '0'}`;
-
+const StatusMessage = ({ winner, current }) => {
   const noMovesLeft = current.board.every(el => el !== null);
 
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `Next player is ${current.isXNext ? 'X' : '0'}`}
-      {!winner && noMovesLeft && 'Match Tie'}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          Next player is{' '}
+          <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}{' '}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span className="text-green">X</span> and{' '}
+          <span className="text-orange">O</span> tied
+        </>
+      )}
+    </div>
   );
 };
 
-export default statusMessage;
+export default StatusMessage;
